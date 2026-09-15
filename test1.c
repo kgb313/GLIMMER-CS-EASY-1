22338938a6d712deba30e25aa793ef62b3aa1881
kgb313 <kgb125678@qq.com>
2026-09-15 08:22:56 +0800

    test.1

--- test1.c
+++ test1.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-int main(){
-
-    printf("心有微光，不惧黑暗\n");
+int main() {
+    int i=0,sum=0;;
+    while(i++<10){
+        sum+=i;
+    }
+    printf("%d\n",sum);
     getchar(); // Wait for user input before closing the console window
     return 0;
 }
